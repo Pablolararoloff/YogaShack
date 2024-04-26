3babeedfcc7b84a0171c1b48df07633ddd3969d2
Pablo Lara Roloff <pablo.lara.roloff@gmail.com>
2024-04-26 11:59:56 +0200

    n

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
       $(".gallery-controls ul li").removeClass("active");
       $(this).addClass("active");
     });
+
     if ($(".gallery-filter").length > 0) {
       var containerEl = document.querySelector(".gallery-filter");
       var mixer = mixitup(containerEl);
@@ -23,14 +24,15 @@
   $(".set-bg").each(function () {
     var bg = $(this).data("setbg");
     $(this).css("background-image", "url(" + bg + ")");
-    $(this).css("background-size", "cover"); // Ensure the background covers the entire element
+    $(this).css("background-size", "cover");
   });
 
-  $(".header-section .nav-menu .mainmenu ul li").on("mousehover", function () {
+  $(".header-section .nav-menu .mainmenu ul li").on("mouseenter", function () {
     $(this).addClass("active");
   });
+
   $(".header-section .nav-menu .mainmenu ul li").on("mouseleave", function () {
-    $(".header-section .nav-menu .mainmenu ul li").removeClass("active");
+    $(this).removeClass("active");
   });
 
   $(".video-popup").magnificPopup({
@@ -43,67 +45,63 @@
 
   $(".show-result-select").niceSelect();
 
-  /*------------------
-     Timetable Filter
-  --------------------*/
   $(".timetable-controls ul li").on("click", function () {
     var tsfilter = $(this).data("tsfilter");
     $(".timetable-controls ul li").removeClass("active");
     $(this).addClass("active");
 
-    if (tsfilter == "all") {
+    if (tsfilter === "all") {
       $(".classtime-table").removeClass("filtering");
       $(".ts-item").removeClass("show");
     } else {
       $(".classtime-table").addClass("filtering");
     }
+
     $(".ts-item").each(function () {
       $(this).removeClass("show");
-      if ($(this).data("tsmeta") == tsfilter) {
+      if ($(this).data("tsmeta") === tsfilter) {
         $(this).addClass("show");
       }
     });
   });
+
   $(".navbar-collapse a").on("click", function () {
     $(".navbar-collapse").collapse("hide");
   });
 
-  // Toggle Navbar on small screens
   $(".navbar-toggler").on("click", function () {
-    var target = $(this).data("target"); // Get the target from data-target attribute
-    $(target).toggleClass("in"); // Toggle the 'in' class to show/hide the navbar
+    var target = $(this).data("target");
+    $(target).toggleClass("in");
   });
 
-  // AOS ANIMATION
   AOS.init({
     disable: "mobile",
     duration: 800,
     anchorPlacement: "center-bottom",
   });
 
-  // SMOOTHSCROLL NAVBAR
   $(function () {
     $(".navbar a, .hero-text a").on("click", function (event) {
       var $anchor = $(this);
-      $("html, body")
-        .stop()
-        .animate(
-          {
-            scrollTop: $($anchor.attr("href")).offset().top - 49,
-          },
-          1000
-        );
-      event.preventDefault();
+      var target = $($anchor.attr("href"));
+
+      if(target.length) {
+        $("html, body").stop().animate({
+          scrollTop: target.offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      }
     });
   });
 
-  // Add scroll event listener for navbar background change
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 0) { // If page is scrolled more than 0px
-        $('.navbar').css('background-color', 'rgb(243, 208, 71)'); // Change navbar background to yellow
-    } else {
-        $('.navbar').css('background-color', ''); // Reset navbar background
-    }
+  $(function () {
+    var $navbar = $('.navbar');
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 0) {
+        $navbar.addClass('navbar-blur');
+      } else {
+        $navbar.removeClass('navbar-blur');
+      }
+    });
   });
-
 })(jQuery);
